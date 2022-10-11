fa14d6787eb3d196ec7a10dc65a3056c8ce6d103
student <student@info-05.cshl.edu>
2022-10-11 10:44:35 -0400

    adding first script sys

---
+++ first_script_sys.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import sys
+name1=sys.argv[1]
+color1=sys.argv[2]
+activity1=sys.argv[3]
+animal1=sys.argv[4]
+
+print('My name is:',name1,'\n','My favorite color is:',color1,'\n','My favorite activity is:',activity1,'\n','My favorite animal is:',animal1)
+ 

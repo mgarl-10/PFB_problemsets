c741f7d6c0d398d7cc67abf9b0bc35a20bffe38c
student <student@info-05.cshl.edu>
2022-10-11 16:57:50 -0400

    python scripts second day

--- python2_script5.py
+++ python2_script5.py
@@ -7,10 +7,14 @@ if int(random_number) > 0:
     print(random_number + " is positive and less than 50")
     if int(random_number) %2==0:
       print(random_number + " is an even number that is smaller than 50")
+    else:
+      print(random_number + " is an odd number that is smaller than 50")
   elif int(random_number)==50:
     print(random_number + " is 50")  
   else:
     if int(random_number) %3==0:
       print(random_number + " is larger than 50 and divisible by 3") 
+    else:
+      print(random_number + " is larger than 50 and indivisible by 3")
 else:
   print(random_number + " is a negative number")

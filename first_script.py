77f95c3d5a7b21e3fa4bd5af1441fb5c1ea5684f
student <student@info-05.cshl.edu>
2022-10-11 10:45:41 -0400

    adding first script

--- first_script.py
+++ first_script.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
  
 my_name='Marleny'
-
+my_color='Blue'
+my_activity='Coding'
+my_animal='Chicken'
 print("My name:", my_name)
-
-
+print("My favourite color:", my_color)
+print("My favorite activity:", my_activity)
+print("My favorite animal:", my_animal)
 

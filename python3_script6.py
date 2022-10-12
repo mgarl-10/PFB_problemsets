010bd874de951324dc9f470b03f1d1a5ce9d5b75
student <student@info-05.cshl.edu>
2022-10-12 12:21:36 -0400

    adding script 6 day 3

--- python3_script6.py
+++ python3_script6.py
@@ -3,7 +3,7 @@ import sys
 dna = sys.argv[1]
 dna_upper=dna.upper()
 sub_dna=dna_upper[99:200]
-GC_counts=sub_dna.count('G')
+G_counts=sub_dna.count('G')
 
-print(f"The substring is {sub_dna} and has {GC_counts} G's")
+print(f"The substring is {sub_dna} and has {G_counts} G's")
 

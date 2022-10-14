aa088576d4bc039af4723829445b154397c466c1
student <student@info-05.cshl.edu>
2022-10-13 22:20:46 -0400

    adding script python 7 day 4

--- python7_script4.py
+++ python7_script4.py
@@ -3,9 +3,14 @@ import re
 import sys
 file1=sys.argv[1]
 headers=0
-ids=r"^>(\w+)"
+#ids=re.findall(r("^>\w+\s?.*",line)
 with open(file1,"r") as file2:
   for line in file2:
-    for i in re.findall(ids,line):
+    for i in re.findall(r"^>\w+\s?.*",line):
       headers+=1
+      it=i
+      print(it)
 print(f"There are {headers} sequences in this file")
+
+
+

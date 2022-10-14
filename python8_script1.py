aeabe4b9d04f0034fc9e4defff8cbc81fe540c81
student <student@info-05.cshl.edu>
2022-10-14 17:05:03 -0400

    adding scripts python 8

---
+++ python8_script1.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+import re
+import sys
+file1=sys.argv[1]
+seqs={}
+
+
+with open(file1,"r") as file2:
+  for line in file2:
+    line=line.rstrip()
+    if re.search(r'^>',line):
+      genes_id=line
+      seqs[genes_id]=""
+    else:
+      seq=line
+      seqs[genes_id] += seq 
+    
+dictionary_counts={}
+for it in seqs:
+  seq=seqs[it]
+  dictionary_counts[it]={'nt_comp':{},'seq':seq}
+  unique=set(seq)
+  for nt in unique:
+    dictionary_counts[it]['nt_comp'][nt]=seq.count(nt)
+
+print(dictionary_counts)
+

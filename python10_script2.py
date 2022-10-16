1b944be2a6fba57c77c5558951ad902d7055d2d0
student <student@info-05.cshl.edu>
2022-10-16 13:34:09 -0400

    adding scripts

--- python10_script2.py
+++ python10_script2.py
@@ -2,21 +2,22 @@
 import re
 import sys
 file1=sys.argv[1]
-start=int(0)
-end=int(60)
+width=sys.argv[2]
+#start=int(0)
+#end=int(60)
 def new_lines(dna,widt):
  list_dna=list(dna)
  start=int(0)
- end=int(60)
+ end=int(widt)
  res=[]
  for line in dna:
   res.append(line.replace("\n",""))
  while dna[start:end]!='':
   print(dna[start:end])  
-  start+=int(60)
-  end+=int(60)
+  start+=int(widt)
+  end+=int(widt)
   
-print(new_lines(file1))
+print(new_lines(file1,width))
 
 
 
--- python2_script3.py
+++ python2_script3.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+
 random_number=56
 
 if random_number > 0:

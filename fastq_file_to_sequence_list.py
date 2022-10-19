b54e067cccec6cf6f3a2a95c4d1c734cc59bae86
student <student@info-05.cshl.edu>
2022-10-18 21:19:29 -0400

    Python RNAseq

---
+++ fastq_file_to_sequence_list.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+
+import os, sys
+import re
+from Bio import SeqIO
+
+## method: seq_list_from_fastq_file(fastq_filename)
+##
+##  Extracts the sequence lines from a fastq file and returns a list
+##  of the sequence lines
+##
+##  input parameters:
+##
+##  fastq_filename :  name of the fastq file (type: string)
+##
+##  returns seq_list : list of read sequences.
+##                    ie.  ["GATCGCATAG", "CGATGCAG", ...]
+    
+def seq_list_from_fastq_file(fastq_filename):
+
+  seq_list = list()
+  linecount=0 
+    ## begin your code
+  with open(fastq_filename) as sequences:
+    for sequence in sequences:
+      sequence=sequence.rstrip()
+      linecount+=1
+      if linecount%4==2:
+        seq_list.append(sequence)
+     
+   
+    
+    ## end your code
+
+    return seq_list
+
+
+
+def main():
+
+    progname = sys.argv[0]
+    
+    usage = "\n\n\tusage: {} filename.fastq num_seqs_show\n\n\n".format(progname)
+    
+    if len(sys.argv) < 3:
+        sys.stderr.write(usage)
+        sys.exit(1)
+
+    # capture command-line arguments
+    fastq_filename = sys.argv[1]
+    num_seqs_show = int(sys.argv[2])
+
+    seq_list = seq_list_from_fastq_file(fastq_filename)
+
+    print(seq_list[0:num_seqs_show])
+
+    sys.exit(0)  # always good practice to indicate worked ok!
+
+
+
+if __name__ == '__main__':
+    main()
+    
